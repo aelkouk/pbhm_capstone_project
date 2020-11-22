9f650d55d29855eaadd8308b2525578a407b709a
Ahmed Elkouk <ahmed.elkouk@edu.uiz.ac.ma>
2020-11-22 12:50:56 +0100

    rewrite jobs.sh to be used with sbatch --array

--- perturb_param/jobs.sh
+++ perturb_param/jobs.sh
@@ -12,9 +12,14 @@ base_path=${HOME}/pbhmCourse_student/2_process_based_modelling
 site_path=${base_path}/settings/plumber/Loobos                             
 summa_settings=${site_path}/summa_zFileManager_Loobos.txt
 SOILPARM_file=${site_path}/SOILPARM.TBL
-sobol_file=${base_path}/perturb_param/SobolPAR_Matrix/SobolPAR_MatrixA.csv # Change to desired file
 summa_exe=${base_path}/perturb_param/summa.exe
 log_file=${base_path}/perturb_param/log_summa.txt
+
+
+sobol_path=${base_path}/perturb_param/SobolPAR_Matrix
+sobol_files=( SobolPAR_MatrixA.csv  SobolPAR_MatrixC1.csv  SobolPAR_MatrixC3.csv SobolPAR_MatrixB.csv  SobolPAR_MatrixC2.csv  SobolPAR_MatrixC4.csv )
+taskid=$SLURM_ARRAY_TASK_ID
+sobol_file=sobol_path/${sobol_files[$taskid-1]}
 
 for row in {1..100}; do
         # Run script chnage SOILTBL using $row value in SobolPAR_Matrix.csv file
@@ -24,7 +29,7 @@ for row in {1..100}; do
         # matlab code 2
         matlab -batch "change_param_all($row,'$sobol_file','$SOILPARM_file')"
         # Run summa with $row as output suffix
-        summa_command="$summa_exe -m $summa_settings --suffix $row"
+        summa_command="$summa_exe -m $summa_settings --suffix $row_$taskid"
 
         # Run as [SUMMA call] > [log file path]
 	$summa_command >> $log_file
--- perturb_param/readme.txt
+++ perturb_param/readme.txt
@@ -4,7 +4,7 @@ Example of preparation required for Loobos site:
 - Reduce the simulation period in summa_zFileManager_Loobos.txt to 2 years
 - Keep 'mLayerVolFracLiq' and 'averageRoutedRunoff' lines in 'summa_zModelOutput.txt' and remove the rest
 - If do not exist, create the SUMMA output directory 2_process_based_modelling/output/plumber/Loobos
-- Sbatch jobs.sh for each of the SobolPAR_Matrix.csv files
+- Sbatch jobs.sh 'sbatch --array=1-6 jobs.sh'
 
 
 
